33abd61b9186f8878ddfb0519a0ce869d2fa7357
sebwyn <gogleboywerser@gmail.com>
2022-10-12 13:07:05 -0400

    small module import changes

--- twoD/src/primitives/line/mod.rs
+++ twoD/src/primitives/line/mod.rs
@@ -3,6 +3,6 @@ mod line;
 mod rect_pipeline;
 
 //expose our lines and vertices to the pipeline at least
-pub use vertex::Vertex;
+pub use vertex::Vertex as LineVertex;
 pub use line::Line;
 pub use rect_pipeline::RectPipeline;
